08abf0d71d955124688a87390a290e9920515b5d
jared.lim <jared.lim@workday.com>
2018-07-21 21:53:35 -0700

    update

--- src/views/Schedule/Schedule.jsx
+++ src/views/Schedule/Schedule.jsx
@@ -35,23 +35,23 @@ class TableList extends Component {
                         <td>02/07/18</td>
                         <td>-</td>
                         <td>-</td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%200%20-%20INTRO.ipynb">LECTURE 0: SETUP</a></td>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%200%20-%20INTRO.ipynb">LECTURE 0: SETUP</a></td>
                         <td>-</td>
                       </tr>
                       <tr>
                         <td>02/14/18</td>
                         <td><b>Lecture 1: Introduction and Data Manipulation</b></td>
                         <td><a href="resources/slides/LECTURE 1.pdf"><img src="resources/slides/images/LECTURE 1.png" class="slide-image" alt="LECTURE 1" /></a></td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%201%20-%20NUMPY.ipynb">LECTURE 1: NUMPY</a><br/>
-                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%201%20-%20PANDAS.ipynb">LECTURE 1: PANDAS</a><br/></td>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%201%20-%20NUMPY.ipynb">LECTURE 1: NUMPY</a><br/>
+                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%201%20-%20PANDAS.ipynb">LECTURE 1: PANDAS</a><br/></td>
                         <td><a href="https://goo.gl/forms/9QzxjSNYqRtj41rm1">Take Home Quiz</a></td>
                       </tr>
                       <tr>
                         <td>02/21/18</td>
                         <td><b>Lecture 2: Manipulation Techniques and Visualization</b></td>
                         <td><a href="resources/slides/LECTURE 2.pdf"><img src="resources/slides/images/LECTURE 2.png" class="slide-image" alt="LECTURE 2" /></a></td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%202%20-%20DEMO.ipynb">LECTURE 2: DEMO</a><br/>
-                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%202%20-%20VISUAL.ipynb">LECTURE 2: VISUAL</a><br/>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%202%20-%20DEMO.ipynb">LECTURE 2: DEMO</a><br/>
+                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%202%20-%20VISUAL.ipynb">LECTURE 2: VISUAL</a><br/>
                             <a href="https://drive.google.com/file/d/1eCJO3K5gg09SR75jbHvIwUKDXQ4L3my9/view?usp=sharing">LECTURE 2: DATASET</a></td>
                         <td><a href="assignments/Project A.pdf">Project A</a></td>
                       </tr>
@@ -59,15 +59,15 @@ class TableList extends Component {
                         <td>02/28/18</td>
                         <td><b>Lecture 3: Introduction to Machine Learning</b></td>
                         <td><a href="resources/slides/LECTURE 3.pdf"><img src="resources/slides/images/LECTURE 3.png" class="slide-image" alt="LECTURE 3" /></a></td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%203%20-%20SUPER.ipynb">LECTURE 3: SUPER</a></td>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%203%20-%20SUPER.ipynb">LECTURE 3: SUPER</a></td>
                         <td><a href="assignments/Project A.pdf">Project A</a></td>
                       </tr>
                       <tr>
                         <td>03/07/18</td>
                         <td><b>Lecture 4: Introduction to Classification</b></td>
                         <td><a href="resources/slides/LECTURE 4.pdf"><img src="resources/slides/images/LECTURE 4.png" class="slide-image" alt="LECTURE 4" /></a></td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%204%20-%20CLASS.ipynb">LECTURE 4: CLASS</a><br/>
-                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%204%20-%20DEMO.ipynb">LECTURE 4: DEMO</a><br/>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%204%20-%20CLASS.ipynb">LECTURE 4: CLASS</a><br/>
+                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%204%20-%20DEMO.ipynb">LECTURE 4: DEMO</a><br/>
                             <a href="https://drive.google.com/file/d/1TOeFfJya4tMEcjXNe2ECAvRsuhse9vBL/view?usp=sharing">LECTURE 4: DATASET</a></td>
                         <td><a href="assignments/Project B.pdf">Project B</a></td>
                       </tr>
@@ -75,39 +75,39 @@ class TableList extends Component {
                         <td>03/14/18</td>
                         <td><b>Lecture 5: SVM</b></td>
                         <td><a href="resources/slides/LECTURE 5.pdf"><img src="resources/slides/images/LECTURE 5.png" class="slide-image" alt="LECTURE 5" /></a></td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%205%20-%20SVM.ipynb">LECTURE 5: SVM</a><br/>
-                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%205%20-%20DEMO.ipynb">LECTURE 5: DEMO</a></td>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%205%20-%20SVM.ipynb">LECTURE 5: SVM</a><br/>
+                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%205%20-%20DEMO.ipynb">LECTURE 5: DEMO</a></td>
                         <td><a href="assignments/Project B.pdf">Project B</a></td>
                       </tr>
                       <tr>
                         <td>03/21/18</td>
                         <td><b>Lecture 6: Logistic Regression and Decision Trees</b></td>
                         <td><a href="resources/slides/LECTURE 6.pdf"><img src="resources/slides/images/LECTURE 6.png" class="slide-image" alt="LECTURE 6" /></a></td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%206%20-%20LOGIT.ipynb">LECTURE 6: LOGIT</a><br/>
-                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%206%20-%20DEMO.ipynb">LECTURE 6: DEMO</a></td>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%206%20-%20LOGIT.ipynb">LECTURE 6: LOGIT</a><br/>
+                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%206%20-%20DEMO.ipynb">LECTURE 6: DEMO</a></td>
                         <td><a href="assignments/Project C.pdf">Project C</a></td>
                       </tr>
                       <tr>
                         <td>03/28/18</td>
                         <td><b>Lecture 7: Unsupervised Learning</b></td>
                         <td><a href="resources/slides/LECTURE 7.pdf"><img src="resources/slides/images/LECTURE 7.png" class="slide-image" alt="LECTURE 7" /></a></td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%207%20-%20UNSUP.ipynb">LECTURE 7: UNSUP</a><br/>
-                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%207%20-%20DEMO.ipynb">LECTURE 7: DEMO</a></td>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%207%20-%20UNSUP.ipynb">LECTURE 7: UNSUP</a><br/>
+                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%207%20-%20DEMO.ipynb">LECTURE 7: DEMO</a></td>
                         <td><a href="assignments/Project C.pdf">Project C</a></td>
                       </tr>
                       <tr>
                         <td>04/11/18</td>
                         <td><b>Lecture 8: Model Optimization</b></td>
                         <td><a href="resources/slides/LECTURE 8.pdf"><img src="resources/slides/images/LECTURE 8.png" class="slide-image" alt="LECTURE 8" /></a></td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%208%20-%20MODEL.ipynb">LECTURE 8: MODEL</a></td>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%208%20-%20MODEL.ipynb">LECTURE 8: MODEL</a></td>
                         <td><a href="assignments/Final Project.pdf">Final Project</a></td>
                       </tr>
                       <tr>
                         <td>04/18/18</td>
                         <td><b>Lecture 9: Cross Validation & Ensemble Learning</b></td>
                         <td><a href="resources/slides/LECTURE 9.pdf"><img src="resources/slides/images/LECTURE 9.png" class="slide-image" alt="LECTURE 9" /></a></td>
-                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%209%20-%20ENSEM.ipynb">LECTURE 9: ENSEM</a><br/>
-                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/JunyoungLim.github.io/blob/master/notes/LECTURE%209%20-%20BAG.ipynb">LECTURE 9: BAG</a></td>
+                        <td><a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%209%20-%20ENSEM.ipynb">LECTURE 9: ENSEM</a><br/>
+                            <a href="http://nbviewer.jupyter.org/github/JunyoungLim/machine-learning-course/blob/master/public/resources/notes/LECTURE%209%20-%20BAG.ipynb">LECTURE 9: BAG</a></td>
                         <td><a href="assignments/Final Project.pdf">Final Project</a></td>
                       </tr>
                     </tbody>
