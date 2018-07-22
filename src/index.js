b0eed555d6123bde737b2bc6ceb1de5e9d3630df
jared.lim <jared.lim@workday.com>
2018-07-21 21:16:17 -0700

    update

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <div>
       {indexRoutes.map((prop, key) => {
           return <Route to={prop.path} component={prop.component} key={key} />;
