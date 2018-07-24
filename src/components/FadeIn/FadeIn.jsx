59ffeac1ca6f7a1d27dda3ae820895cba21cce2d
jared.lim <jared.lim@workday.com>
2018-07-23 17:25:09 -0700

    Header effect finalized

---
+++ src/components/FadeIn/FadeIn.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class FadeIn extends Component {
+  state = {
+    maxIsVisible: 0
+  };
+
+  get delay() {
+    return this.props.delay || 50;
+  }
+
+  get transitionDuration() {
+    return this.props.transitionDuration || 400;
+  }
+
+  componentDidMount() {
+    const count = React.Children.count(this.props.children);
+    let i = 0;
+    this.interval = setInterval(() => {
+      i++;
+      if (i > count) clearInterval(this.interval);
+
+      this.setState({ maxIsVisible: i });
+    }, this.delay);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const transitionDuration = this.transitionDuration;
+    return (
+      <div>
+        {React.Children.map(this.props.children, (child, i) => {
+          return (
+            <div
+              style={{
+                transition: `opacity ${transitionDuration}ms, top ${transitionDuration}ms`,
+                position: "relative",
+                top: this.state.maxIsVisible > i ? 0 : 20,
+                opacity: this.state.maxIsVisible > i ? 1 : 0
+              }}
+            >
+              {child}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default FadeIn;
--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -6,59 +6,61 @@ import FadeIn from "react-fade-in";
 class HeaderLinks extends Component {
   render() {
     return (
-      <FadeIn transitionDuration={1000}>
-        <Navbar inverse collapseOnSelect fixedTop={true}>
-          <Navbar.Header>
-            <a href=".">
-            <Navbar.Brand>
-              <img src="cds-logo.png" alt="CDS Edu Logo" />
-            </Navbar.Brand>
-            </a>
-            <Navbar.Brand>
-            <a href=".">CDS Education</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <NavItem>
-                <NavLink
-                  to="/started"
-                  className="nav-link"
-                  activeClassName="active">
-                  Getting Started
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  to="/schedule"
-                  className="nav-link"
-                  activeClassName="active">
-                  Schedule
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  to="/syllabus"
-                  className="nav-link"
-                  activeClassName="active">
-                  Syllabus
-                </NavLink>
-              </NavItem>
-              <NavDropdown eventKey={3} title="Resources" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Tutorial</MenuItem>
-                <MenuItem eventKey={3.2} href="https://cmsx.cs.cornell.edu/" target="_blank" rel="noopener noreferrer">CMS</MenuItem>
-                <MenuItem eventKey={3.3} href="https://piazza.com/" target="_blank" rel="noopener noreferrer">Piazza</MenuItem>
-              </NavDropdown>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="https://cornelldata.science/">
-                Cornell Data Science
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-            </Navbar>
-      </FadeIn>
+      <Navbar inverse collapseOnSelect fixedTop={true}>
+        <FadeIn transitionDuration={1000}>
+          <div>
+            <Navbar.Header>
+              <a href=".">
+              <Navbar.Brand>
+                <img src="cds-logo.png" alt="CDS Edu Logo" />
+              </Navbar.Brand>
+              </a>
+              <Navbar.Brand>
+              <a href=".">CDS Education</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem>
+                  <NavLink
+                    to="/started"
+                    className="nav-link"
+                    activeClassName="active">
+                    Getting Started
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    to="/schedule"
+                    className="nav-link"
+                    activeClassName="active">
+                    Schedule
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    to="/syllabus"
+                    className="nav-link"
+                    activeClassName="active">
+                    Syllabus
+                  </NavLink>
+                </NavItem>
+                <NavDropdown eventKey={3} title="Resources" id="basic-nav-dropdown">
+                  <MenuItem eventKey={3.1}>Tutorial</MenuItem>
+                  <MenuItem eventKey={3.2} href="https://cmsx.cs.cornell.edu/" target="_blank" rel="noopener noreferrer">CMS</MenuItem>
+                  <MenuItem eventKey={3.3} href="https://piazza.com/" target="_blank" rel="noopener noreferrer">Piazza</MenuItem>
+                </NavDropdown>
+              </Nav>
+              <Nav pullRight>
+                <NavItem eventKey={1} href="https://cornelldata.science/">
+                  Cornell Data Science
+                </NavItem>
+              </Nav>
+            </Navbar.Collapse>  
+          </div>
+        </FadeIn>
+      </Navbar>
     );
   }
 }
